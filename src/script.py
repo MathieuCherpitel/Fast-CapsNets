3b7cf553f509383c4d92e96db7d6c2bde6c4bcf6
Mathieu Cherpitel <mc2091@hw.ac.uk>
2023-07-21 12:16:19 +0100

    [*] change size of training and testing data

--- src/script.py
+++ src/script.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     config = vars(args)
 
     if args.dataset == "mnist":
-        train_data, test_data = load_mnist()
+        train_data, test_data = load_mnist(size=(2000, 500))
     elif args.dataset == "fmnist":
         train_data, test_data = load_fashion_mnist()
     elif args.dataset == "cifar10":
